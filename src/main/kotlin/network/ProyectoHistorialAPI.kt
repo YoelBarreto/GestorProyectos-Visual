8d6366cbc301f2b667ad5109da956f7bd1a3c4b9
YoelBarreto <yobelb8@gmail.com>
2025-02-06 13:28:07 +0000

    Creando la API para llamar al historial de proyectos pasados de fecha

--- src/main/kotlin/network/ProyectoHistorialAPI.kt
+++ src/main/kotlin/network/ProyectoHistorialAPI.kt
@@ -1,2 +1,27 @@
 package network
 
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import model.Proyecto
+import network.NetworkUtils.httpClient
+
+fun apiPastProyects(onSuccessResponse: (List<Proyecto>) -> Unit) {
+    val url = "http://127.0.0.1:5000/proyectos/historial"
+
+    CoroutineScope(Dispatchers.IO).launch {
+        val response = httpClient.get(url) {
+            contentType(ContentType.Application.Json)
+        }
+        if (response.status == HttpStatusCode.OK) {
+            val listProyecto = response.body<List<Proyecto>>()
+            onSuccessResponse(listProyecto)
+        } else {
+            println("Error: ${response.status}, Body: ${response.bodyAsText()}")
+        }
+    }
+}
